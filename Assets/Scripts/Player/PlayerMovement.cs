466e59b5452cc5d0cabc1271989516c531b6ebb7
Dominic Griffith <dgriffith6227@sdsu.edu>
2022-05-11 21:14:55 -0700

    Added walking, running, crouching, and changed InputManager button names to match commands

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,50 +5,88 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //add reference in editor
+    [Header("References")]
     public Rigidbody rb;
     public LayerMask groundMask;
     public CapsuleCollider playerBody;
 
-    public float speed = 120f;
+
+    [Header("Movement")]
+    public float walkSpeed = 70;
+    public float sprintSpeed = 100;
+    public MovementState movementState;
     public float groundDrag = 5f;
+
+    private float x;
+    private float z;
+    private float speed;
+    private float playerHeight;
+    private bool isGrounded;
+    
+
+    [Header("Jumping")]
     public float jumpForce = 12f;
     public float jumpCoolDown = 0.25f;
     public float airMultiplier = 0.4f;
 
-    private float playerHeight;
-    private float x;
-    private float z;
-    private bool isGrounded;
     private bool isJumping;
+
+
+    [Header("Crouching")]
+    public float crouchSpeed = 35f;
+    public float crouchYScale = 0.5f;
+
+    private float startYScale;
+
+
 
     private void Start()
     {
         playerHeight = playerBody.height;
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundMask);
 
-        SpeedControl();
+        PlayerInput();
 
-        //get player input
+        SpeedControl();
+        StateHandler();
+        ApplyDrag();
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter();
+    }
+
+    private void PlayerInput()
+    {
         x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
 
-        ApplyDrag();
-
+        //applys jump to player and also ensures they cant jump infinitely
         if (Input.GetButtonDown("Jump") && isGrounded && !isJumping)
         {
             isJumping = true;
             Jump();
             Invoke(nameof(ResetJump), jumpCoolDown);
         }
-    }
 
-    private void FixedUpdate()
-    {
-        moveCharacter();
+        //starts crouch
+        if (Input.GetButtonDown("Crouch"))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        //stop crouch
+        if (Input.GetButtonUp("Crouch"))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
 
     private void moveCharacter()
@@ -94,5 +132,39 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         isJumping = false;
+    }
+
+    private void StateHandler()
+    {
+        //Crouching
+        if (Input.GetButton("Crouch"))
+        {
+            movementState = MovementState.crouching;
+            speed = crouchSpeed;
+        }
+        //Sprinting
+        if(isGrounded && Input.GetButton("Sprint"))
+        {
+            movementState = MovementState.sprinting;
+            speed = sprintSpeed;
+        }
+        //Walking
+        else if(isGrounded) {
+            movementState = MovementState.walking;
+            speed = walkSpeed;
+        }
+        //Air
+        else
+        {
+            movementState = MovementState.air;
+        }
+    }
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
     }
 }
